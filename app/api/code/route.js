e9169a086cac20403924fb0c06612ff0b0eadff4
shivup5005 <shivup5005@gmail.com>
2024-04-08 18:06:02 +0000

    commit

--- app/api/code/route.js
+++ app/api/code/route.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useState } from "react";
 
 export const SendMessage = (message) => {
     // [messagedata,setMessageData] = useState()
--- app/api/image/route.js
+++ app/api/image/route.js
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react"
 import OpenAI from "openai"
-
-
-
-
-
 
 
  
